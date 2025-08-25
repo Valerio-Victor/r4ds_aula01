588beaf98e61fabc6c7304661154428d050b4271
Victor Valerio <victor.dmv@unifei.edu.br>
2025-08-25 13:03:12 -0300

    Initial commit

---
+++ script_aula_01.R
@@ -0,0 +1,78 @@
+## FUNÇÕES MATEMÁTICAS
+
+# Função de Adição
+1+1 # "+"
+# Função de Subtração
+2-1 # "-"
+# Função de Multiplicação
+2*2 # "*"
+# Função de Divisão
+3/2 # "/"
+# Função Potenciação
+2^3 # "^"
+
+# FUNÇÕES DE MANIPULÇÃO DE DADOS 
+# nome_da_funcao(argumentos)
+c(5,10,15) # combinar valores 
+
+print("O Palmeiras possui 1 Mundial") # Imprime no console o texto do argumento 
+
+# FUNÇÕES ESTATÍSTICAS
+# nome_da_funcao(argumentos)
+(5 + 10 + 15)/3
+
+mean(c(5,10,15)) # A função mean calcula a média de um conjunto de valores 
+
+# Atividade
+# Imprima no console com a função print o resultado da média dos valores com a 
+# função mean
+
+print(c("A média é:", mean(c(5,10,15))))
+
+# OBJETO
+# Estrutura de armazenamento de dados (valor, texto, resultado de uma função, ...)
+
+# Objeto armazena a informação de valor
+valor <- 10
+
+# Objeto armazena a informação de texto
+texto <- "O palmeiras possui mundial"
+
+# Objeto resultado de uma operação
+adicao <- 1+1
+adicao_2 <- adicao + 1
+
+# 
+adicao <- 1+1
+adicao <- 1+5
+
+# Objeto resultado de uma função
+media <- mean(c(5,10,15))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
